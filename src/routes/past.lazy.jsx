255e0bab71b8e5b393a6e5f784a03a3dafdcf549
Ben Zeise <ben.zeise@gmail.com>
2024-10-30 16:05:54 -0500

    through 16-use

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Suspense, useState, use } from "react"; // import Suspense and use
 import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import getPastOrders from "../api/getPastOrders";
@@ -11,9 +11,27 @@ export const Route = createLazyFileRoute("/past")({
 });
 
 function ErrorBoundaryWrappedPastOrderRoutes() {
+  const [page, setPage] = useState(1);
+  const loadedPromise = useQuery({
+    queryKey: ["past-orders", page],
+    queryFn: () => getPastOrders(page),
+    staleTime: 30000,
+  }).promise;
   return (
     <ErrorBoundary>
-      <PastOrdersRoute />
+      <Suspense
+        fallback={
+          <div className="past-orders">
+            <h2>Loading Past Orders …</h2>
+          </div>
+        }
+      >
+        <PastOrdersRoute
+          loadedPromise={loadedPromise}
+          page={page}
+          setPage={setPage}
+        />
+      </Suspense>
     </ErrorBoundary>
   );
 }
@@ -23,14 +41,15 @@ const intl = new Intl.NumberFormat("en-US", {
   currency: "USD",
 });
 
-function PastOrdersRoute() {
-  const [page, setPage] = useState(1);
+function PastOrdersRoute({ page, setPage, loadedPromise }) {
+  const data = use(loadedPromise);
+  // const [page, setPage] = useState(1);
   const [focusedOrder, setFocusedOrder] = useState();
-  const { isLoading, data } = useQuery({
-    queryKey: ["past-orders", page],
-    queryFn: () => getPastOrders(page),
-    staleTime: 30000,
-  });
+  // const { isLoading, data } = useQuery({
+  //   queryKey: ["past-orders", page],
+  //   queryFn: () => getPastOrders(page),
+  //   staleTime: 30000,
+  // });
 
   const { isLoading: isLoadingPastOrder, data: pastOrderData } = useQuery({
     queryKey: ["past-order", focusedOrder],
@@ -39,13 +58,6 @@ function PastOrdersRoute() {
     staleTime: 24 * 60 * 60 * 1000, // one day in milliseconds,
   });
 
-  if (isLoading) {
-    return (
-      <div className="past-orders">
-        <h2>LOADING …</h2>
-      </div>
-    );
-  }
   return (
     <div className="past-orders">
       <table>
