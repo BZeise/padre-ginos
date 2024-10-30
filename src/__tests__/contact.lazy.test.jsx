b68df54b049554616127b19094ab2a04697936b8
Ben Zeise <ben.zeise@gmail.com>
2024-10-30 14:06:09 -0500

    through testing-user-interaction

---
+++ src/__tests__/contact.lazy.test.jsx
@@ -0,0 +1,57 @@
+import { render } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Route } from "../routes/contact.lazy";
+
+const queryClient = new QueryClient();
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+test("can submit contact form", async () => {
+  fetchMocker.mockResponse(JSON.stringify({ status: "ok" }));
+  const screen = render(
+    <QueryClientProvider client={queryClient}>
+      <Route.options.component />
+    </QueryClientProvider>,
+  );
+
+  const nameInput = screen.getByPlaceholderText("Name");
+  const emailInput = screen.getByPlaceholderText("Email");
+  const msgTextArea = screen.getByPlaceholderText("Message");
+
+  const testData = {
+    name: "Brian",
+    email: "test@example.com",
+    message: "This is a test message",
+  };
+
+  nameInput.value = testData.name;
+  emailInput.value = testData.email;
+  msgTextArea.value = testData.message;
+
+  const btn = screen.getByRole("button");
+
+  btn.click();
+
+  // we add this in because we need to wait for the button to finish its click action
+  // we have chosen to await an h3 because that's what the user will see when the action is complete
+  const h3 = await screen.findByRole("heading", { level: 3 });
+
+  // .toContain is less specific than .toBe
+  expect(h3.innerText).toContain("Submitted");
+  // could end here
+
+  // but now let's check and see if we called the API endpoint correctly
+  const requests = fetchMocker.requests();
+  expect(requests.length).toBe(1);
+  expect(requests[0].url).toBe("/api/contact");
+  expect(fetchMocker).toHaveBeenCalledWith("/api/contact", {
+    body: JSON.stringify(testData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+});
